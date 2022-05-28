7437fdf39a1432df6862e1b3c7c52a6b39b32c67
monojw <monojw.io@gmail.com>
2022-05-28 22:46:29 +0900

    SearchInput 컴포넌트 수정

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-
-import { Input } from 'antd';
 import { search } from '../slices/Search';
-const { Search } = Input;
+
+import { Button, Form, Input } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 
 const SearchInputContainer = styled.div`
   display: flex;
@@ -19,11 +19,16 @@ const SearchInputContainer = styled.div`
     font-size: 1rem;
   }
 
+  .ant-form {
+    display: flex;
+    width: 100%;
+  }
+
   .ant-input-affix-wrapper {
     font-size: 1.5rem;
   }
 
-  .ant-input-search-large .ant-input-search-button {
+  .ant-btn {
     height: 52px;
   }
 `;
@@ -36,29 +41,43 @@ const SearchInput = ({ children }) => {
     q: 'track:', // 검색어
     type: 'track', // track으로 검색
     limit: '3', // 검색결과 3개 노출
-    token: token, // 토큰
   });
 
   // 검색 이벤트
-  const onSearch = (value) => {
-    const nextParams = {
-      ...params,
-      q: 'track:' + value, //검색어
-    };
-    setParams(nextParams);
-  };
+  const onSearch = useCallback(
+    (e) => {
+      const nextParams = {
+        ...params,
+        q: 'track:' + e.target.value, //검색어
+        token: token, // 토큰
+      };
+      setParams(nextParams);
+    },
+    [params, token],
+  );
 
-  useEffect(() => {
+  // 검색 submit 이벤트
+  const onSubmit = useCallback(() => {
     dispatch(search(params)); // 새로운 값을 dispatch한다.
-  }, [params]);
+  }, [dispatch, params]);
 
   return (
     <SearchInputContainer>
       {children}
       {/* SearchInput 영역 */}
-      <Search placeholder="제목을 입력하세요." allowClear enterButton="Search" size="large" onSearch={onSearch} />
+      <Form onFinish={onSubmit}>
+        <Input
+          placeholder="제목을 입력하세요."
+          allowClear
+          size="large"
+          onChange={onSearch}
+        />
+        <Button type="primary" htmlType="submit" icon={<SearchOutlined />}>
+          Search
+        </Button>
+      </Form>
     </SearchInputContainer>
   );
 };
 
-export default SearchInput;
+export default React.memo(SearchInput);
