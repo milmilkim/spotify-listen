7c22664675fa3b3b4f6b63350970ac599cf6ceb1
milmilkim <soundkim27@gmail.com>
2022-05-29 18:22:56 +0900

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,14 @@
-import { redirect_uri } from '../config';
 import { Button } from 'antd';
 
 const client_id = process.env.REACT_APP_CLIENT_ID;
-
+const redirect_url = process.env.REACT_APP_REDIRECT_URL;
 const scope = 'user-read-private user-read-email user-top-read';
 
 let url = 'https://accounts.spotify.com/authorize';
 url += '?response_type=token';
 url += '&client_id=' + encodeURIComponent(client_id);
 url += '&scope=' + encodeURIComponent(scope);
-url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+url += '&redirect_uri=' + encodeURIComponent(redirect_url);
 
 function Login() {
   return (
