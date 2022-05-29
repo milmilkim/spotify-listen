cfda9a2afbb7b5ae1045892938c6f5edc0b6d0fd
monojw <monojw.io@gmail.com>
2022-05-29 17:56:21 +0900

    Chart 컴포넌트

---
+++ src/components/Chart.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { ResponsiveRadar } from '@nivo/radar';
+
+const Chart = ({ data }) => {
+  return (
+    <div style={{ width: '600px', height: '600px' }}>
+      <ResponsiveRadar
+        data={data}
+        keys={['value']}
+        indexBy="label"
+        maxValue={1}
+        valueFormat=">-.2f"
+        margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+        borderColor={{ from: 'color' }}
+        gridLabelOffset={36}
+        dotSize={10}
+        dotColor={{ theme: 'background' }}
+        dotBorderWidth={2}
+        colors={['#1ED760']}
+        blendMode="multiply"
+        motionConfig="wobbly"
+        legends={[
+          {
+            anchor: 'top-left',
+            direction: 'column',
+            translateX: -50,
+            translateY: -40,
+            itemWidth: 80,
+            itemHeight: 20,
+            itemTextColor: '#999',
+            symbolSize: 12,
+            symbolShape: 'circle',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemTextColor: '#000',
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default Chart;
