28192a58563ac1e0f18c6d1a4fe56d85cacd8600
smile <smilemet@gmail.com>
2022-05-29 14:07:38 +0900

    CSS, 라우터 수정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Receipt from '../pages/Receipt';
-import Search from '../pages/Search';
-import Recommend from '../pages/Recommend';
-import RecommendResult from '../pages/RecommendResult';
-import SearchResult from '../pages/SearchResult';
-import Main from '../pages/Main';
+import Receipt from "../pages/Receipt";
+import Search from "../pages/Search";
+import Recommend from "../pages/Recommend";
+import RecommendResult from "../pages/RecommendResult";
+import SearchResult from "../pages/SearchResult";
+import Main from "../pages/Main";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/receipt" element={<Receipt />} />
       <Route path="/recommend" element={<Recommend />} />
       <Route path="/recommend_result" element={<RecommendResult />} />
       <Route path="/search" element={<Search />} />
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import store from './store';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import Meta from './Meta';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import store from "./store";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
+import Meta from "./Meta";
+import GlobalStyles from "./GlobalStyles";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles />
+      <Meta />
       <BrowserRouter>
-        <Meta />
         <App />
       </BrowserRouter>
     </Provider>
